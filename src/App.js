b6b00555769dff890d26992bc53bc4a43d0336d3
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2023-05-01 19:01:59 -0300

    Tentativa de Suicidio após a falha em conseguir colocar o CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Hooks from './components/Hooks';
-import AnotherComponent from './components/AnottherComponent';
-import Images from './components/Images';
-import List from './components/List';
-import RenderCond from './components/RenderCond';
-import Fragment from './components/Fragment';
-import Container from './components/Container';
 import './App.css';
-import './index.css'
 
 function App() {
   return (
     <div className="App">
-        <h1>TODOa</h1>
+        <h1>TODO</h1>
     </div>
   );
 }
