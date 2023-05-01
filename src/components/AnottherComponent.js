a23730c3191410b620a215b1302a9d6974c4d519
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2023-05-01 17:21:08 -0300

    Another Component

---
+++ src/components/AnottherComponent.js
@@ -0,0 +1,16 @@
+const AnotherComponent = () =>{
+    const handleClick = () =>{
+        console.log("Clicou no botão")
+    };
+
+    return(
+        <div>
+            <p>Segundo Componente</p>
+            <button onClick={handleClick}>Evento de Click</button>
+            <hr></hr>
+            <button onClick={() => console.log("Teste")}>Evento no elemento</button>
+        </div>
+    );
+};
+
+export default AnotherComponent;
