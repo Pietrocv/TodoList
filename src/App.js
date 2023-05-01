079a8e492e3083510e8bffaf61818cbd9f495523
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2023-05-01 17:41:27 -0300

    Aprendendo a como chamar as imagens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import AnotherComponent from './components/AnottherComponent';
+import Images from './components/Images';
 import './App.css';
 
 function App() {
   return (
-    <><div>TESTE</div>
-    <AnotherComponent/></>
+    <div>TESTE
+      <AnotherComponent />
+      <Images />
+    </div>
   );
 }
 
---
+++ src/components/Images.js
@@ -0,0 +1,12 @@
+import london from "../assets/London.jpg"
+
+const Images = () =>{
+    return(
+        <div>
+            <img src="/Foto.jpg"></img>
+            <img src={london}></img>
+        </div>
+    )
+}
+
+export default Images;
