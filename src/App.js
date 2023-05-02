93b9e88a38cf2112c68408b0257c1cc278e2a1e6
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2023-05-01 21:40:03 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookMarkCheckFill} from "react-icons/bs";
+
+const API ="http://localhost:5000"
 
 function App() {
   return (
